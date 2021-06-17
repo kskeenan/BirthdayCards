026ced6087fe1e961044c2d71b4c0ede3ead2740
kskeenan <kskeenan@uw.edu>
2021-06-17 00:46:40 -0700

    Add project files.

---
+++ BirthdayCards/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayCards.Models;
+
+namespace BirthdayCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Display the view (empty form)
+        [HttpGet]
+        public IActionResult CardForm()
+        {
+            return View(new Card());
+        }
+
+        // Receive the data coming from the browser
+        [HttpPost]
+        public IActionResult CardForm(Models.Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Sent", card);
+            }
+            else
+            {
+                return View(card);
+            }
+
+        }
+    }
+}
---
+++ BirthdayCards/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCards.Models
+{
+    public class Card
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string FromName { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string ToName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+    }
+}
